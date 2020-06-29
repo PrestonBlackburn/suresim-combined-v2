c5882cd3768a27b0fac69ed513f1e07908baf4a7
PrestonBlackburn <prestonblckbrn@gmail.com>
2020-06-29 13:13:03 -0600

    added a rough api page for pulling EOS values

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from flask import Flask, render_template, redirect, url_for, request
 from shrinkage.shrinkage_csv import shrinkage_csv
 from EOS.EOS_app import EOS_app
 from scaling.scaling_app import scaling_app
+from EOS_API.EOS_api_app import EOS_api_app
 
 
 app = Flask(__name__)
 app.register_blueprint(shrinkage_csv, url_prefix = "/shrinkage")
 app.register_blueprint(EOS_app, url_prefix = "/EOS" )
+app.register_blueprint(EOS_api_app, url_prefix="/EOS/API")
 app.register_blueprint(scaling_app, url_prefix = "/scaling" )
+
 
 
 @app.route("/")
